4635b501566ce7434e47fb6dda036010173bd1f0
Luca D'Amico <menteora@gmail.com>
2026-09-07 10:06:04 +0200

    Add Tampermonkey metadata endpoint

---
+++ gmail-superpowers.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Gmail Superpowers
+// @namespace    https://github.com/menteora/gmail-superpowers
+// @version      0.2.1
+// @description  Copy a portable Gmail subject search as URL or Markdown from message rows and opened emails.
+// @author       menteora
+// @match        https://mail.google.com/mail/*
+// @grant        GM_setClipboard
+// @run-at       document-idle
+// @homepageURL  https://github.com/menteora/gmail-superpowers
+// @supportURL   https://github.com/menteora/gmail-superpowers/issues
+// @updateURL    https://raw.githubusercontent.com/menteora/gmail-superpowers/main/gmail-superpowers.meta.js
+// @downloadURL  https://raw.githubusercontent.com/menteora/gmail-superpowers/main/gmail-superpowers.user.js
+// ==/UserScript==
